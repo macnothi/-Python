824f4df077790897034f9e02bffd5ef84ffad92e
macnothi <macnothi@me.com>
2024-01-01 22:59:09 +0100

    comments

--- blink_timer.py
+++ blink_timer.py
@@ -1,11 +1,15 @@
 from machine import Pin, Timer
 
-led = Pin("LED",Pin.OUT)
+led = Pin("LED",Pin.OUT) # will work on Pico W only
 tim = Timer()
 
-def tick():
+# timer callback
+# The callback must take one argument, which is passed the Timer object. 
+# The callback argument shall be specified. 
+# Otherwise an exception will occur upon timer expiration
+def tick(t):
     global led
     led.toggle()
 
-tim.init(freq=2.5, mode=Timer.PERIODIC, callback=tick())
- 
+# start timer at 2.5Hz, calling tick periodic
+tim.init(freq=2.5, mode=Timer.PERIODIC, callback=tick)
