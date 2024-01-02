9f0ad9dc76d8e555d2d1274de26a53945a4535ea
macnothi <macnothi@me.com>
2024-01-03 00:28:27 +0100

    scan networks ready

--- WIFI/wifimgr.py
+++ WIFI/wifimgr.py
@@ -6,11 +6,10 @@ sta.active(True)
 networks = sta.scan()
 
 # laut docu eigentlich 0..4, man bekommt aber 5 für WPA/WPA2-PSK?
-SECURITY = dict([(1,'open'), (2,'WEP'), (3,'WPA-PSK'), (4,'WPA2-PSK'), (5,'WPA/WPA2-PSK')])
+SECURITY = dict([(1,'OPEN'), (2,'WEP'), (3,'WPA-PSK'), (4,'WPA2-PSK'), (5,'WPA/WPA2-PSK')])
 #HIDDEN = dict([(0,'visible'),(1,'hidden')])
 
-for ssid, bssid, channel, rssi, security, visibility in sorted(networks):
+for ssid, bssid, channel, rssi, security, visibility in networks:
     ssid = ssid.decode('UTF-8')
-    encrypted = security > 1
-    print("ssid: %s, chan: %d, encrypted: %s, encryption: %s" % (ssid,channel,encrypted, SECURITY.get(security,'?')))
+    print("ssid: %s, chan: %d, encryption: %s" % (ssid, channel, SECURITY.get(security,'?')))
 
