af66c7136bdbd2f94ffd3388775becb9bdc50e07
macnothi <macnothi@me.com>
2024-01-02 23:16:04 +0100

    scans

--- WIFI/wifimgr.py
+++ WIFI/wifimgr.py
@@ -6,6 +6,9 @@ sta.active(True)
 
 networks = sta.scan()
 
-SECURITY = {0: 'open', 1: "WEP", 2: "WPA-PSK", 3: "WPA2-PSK", 4: "WPA/WPA2-PSK"}
-HIDDEN = {0: 'visible', 1: 'hidden'}
+SECURITY = dict([(0,'open'), (1,'WEP'), (2,'WPA-PSK'), (3,'WPA2-PSK'), (4,'WPA/WPA2-PSK')])
+HIDDEN = dict([(0,'visible'),(1,'hidden')])
 
+for ssid, bssid, channel, rssi, security, hidden in networks:
+    ssid = ssid.decode('UTF-8')
+    print("ssid: %s, chan: %d, security: %d, visibility: %d" %(ssid,channel,security,hidden))
