50e341a6ecb32584238c49eba7dd10e9230069dd
macnothi <macnothi@me.com>
2024-01-03 00:06:56 +0100

    jo

--- WIFI/wifimgr.py
+++ WIFI/wifimgr.py
@@ -3,12 +3,14 @@ import network
 # enable station interface and connect to WiFi access point
 sta = network.WLAN(network.STA_IF)
 sta.active(True)
-
 networks = sta.scan()
 
-SECURITY = dict([(0,'open'), (1,'WEP'), (2,'WPA-PSK'), (3,'WPA2-PSK'), (4,'WPA/WPA2-PSK')])
-HIDDEN = dict([(0,'visible'),(1,'hidden')])
+# laut docu eigentlich 0..4, man bekommt aber 5 für WPA/WPA2-PSK?
+SECURITY = dict([(1,'open'), (2,'WEP'), (3,'WPA-PSK'), (4,'WPA2-PSK'), (5,'WPA/WPA2-PSK')])
+#HIDDEN = dict([(0,'visible'),(1,'hidden')])
 
-for ssid, bssid, channel, rssi, security, hidden in networks:
+for ssid, bssid, channel, rssi, security, visibility in sorted(networks):
     ssid = ssid.decode('UTF-8')
-    print("ssid: %s, chan: %d, security: %d, visibility: %d" %(ssid,channel,security,hidden))
+    encrypted = security > 1
+    print("ssid: %s, chan: %d, encrypted: %s, encryption: %s" % (ssid,channel,encrypted, SECURITY.get(security,'?')))
+
